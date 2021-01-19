d740898d02e001c75cb105df78fe0dc86d3957a9
Julian Arroyo <jaac8@hotmail.com>
2021-01-19 14:40:38 -0500

    Commint Creacion menu y opciones principales

---
+++ app/src/main/java/com/jarroyo/pokemondata/Views/MainActivity.java
@@ -0,0 +1,78 @@
+package com.jarroyo.pokemondata.Views;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+import com.jarroyo.pokemondata.Interfaces.iComunicaFragments;
+import com.jarroyo.pokemondata.R;
+import com.jarroyo.pokemondata.Utils.Mensaje;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
+        iComunicaFragments {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
+
+    SweetAlertDialog sweetAlertDialog;
+    Mensaje mensaje;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        sweetAlertDialog = new SweetAlertDialog(this);
+        mensaje = new Mensaje();
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        //para cerrar automaticamente el menu
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+
+        switch(menuItem.getItemId()){
+            case R.id.listPokemon:
+
+                break;
+            case R.id.listBerries:
+
+                break;
+            case R.id.salir:
+                sweetAlertDialog = mensaje.MensajeConfirmacionAdvertenciaConUnBoton(this,"Mensaje",
+                        "Desea cerrar el aplicativo?");
+                sweetAlertDialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(SweetAlertDialog sweetAlertDialog) {
+                        finish();
+                    }
+                });
+                break;
+        }
+        return false;
+    }
+
+}
